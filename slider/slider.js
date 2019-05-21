f2b215d99f61fc20c08b242bc86913b06d2e8d3f
Svanhild Egge <svanhildve@gmail.com>
2019-05-21 10:57:37 +0200

    Made the scrolling work!

--- slider/slider.js
+++ slider/slider.js
@@ -3,15 +3,19 @@ const paginationTags = document.querySelectorAll(".pagination .pagination__link"
 
 let currentSlide = 0
 
-
-
-
 const nextSlide = function(){
 
 	if (currentSlide < slideTags.length - 1) {
 		slideTags[currentSlide].classList.remove("active")
+		paginationTags[currentSlide].classList.remove("active")
+
 		currentSlide = currentSlide + 1
+
 		slideTags[currentSlide].classList.add("active")
+		paginationTags[currentSlide].classList.add("active")
+		
+
+		
 	} else {
 		currentSlide = currentSlide
 	}
@@ -21,8 +25,13 @@ const prevSlide = function(){
 
 	if (currentSlide > 0) {
 		slideTags[currentSlide].classList.remove("active")
+		paginationTags[currentSlide].classList.remove("active")
+
 		currentSlide = currentSlide - 1
+
 		slideTags[currentSlide].classList.add("active")
+		paginationTags[currentSlide].classList.add("active")
+		
 	} else {
 		currentSlide = currentSlide
 	}
@@ -42,7 +51,34 @@ document.querySelector(".linkPrev").addEventListener("click", function(){
 })
 
 
-// pagination
+// scroll
+
+function debounced(delay, fn) {
+  let timerId;
+  return function (...args) {
+    if (timerId) {
+      clearTimeout(timerId);
+    }
+    timerId = setTimeout(() => {
+      fn(...args);
+      timerId = null;
+    }, delay);
+  }
+}
+
+const myHandler = (event) => { 
+	if (event.deltaY < 0) {
+		console.log('scrolling up')
+		prevSlide()
+	} 
+	if (event.deltaY > 0) {
+		console.log('scrolling down')
+		nextSlide() 
+	}
+} // do something with the event
+
+const dHandler = debounced(50, myHandler);
+document.addEventListener("wheel", dHandler);
 
 
-
+//
