8432f7d89068e80131508f4417bb5cb11b5a3c9f
Jo <Jo@MacBook-Pro.local>
2019-05-26 21:39:22 +0200

    Add pagination to slider

--- slider/slider.js
+++ slider/slider.js
@@ -1,4 +1,30 @@
 const slideTags = document.querySelectorAll(".projects section")
+const paging = document.querySelector(".pagination");
+
+let count = 0;
+paging.firstElementChild.id = 1;
+
+//foreach slide in projects section
+slideTags.forEach(function(element){
+	//increase count by one
+	count++
+	//ignore first button as it already exists 
+	if (count > 1){
+		//dynamically create new paging link foreach slide 
+		let pagerBtn = document.createElement('a')
+		pagerBtn.href = "#";
+		pagerBtn.id = count;
+		pagerBtn.classList.add('pagination__link');
+		//dynamically create spans inside each link
+		let pagerSpan = document.createElement('span');
+		pagerSpan.innerHTML = "Side " + count;
+		pagerSpan.classList.add('sr-only');
+		//append <a> and <span> elements
+		paging.appendChild(pagerBtn);
+		pagerBtn.appendChild(pagerSpan);
+	}
+})
+
 const paginationTags = document.querySelectorAll(".pagination .pagination__link")
 
 let currentSlide = 0
@@ -89,50 +115,16 @@ const myHandler = (event) => {
 const dHandler = debounced(50, myHandler);
 document.addEventListener("wheel", dHandler);
 
+paginationTags.forEach(function(tag) {
+	tag.addEventListener("click", function() {
+	  //pass clicked element to goToSlide function
+	  goToSlide(tag)
+	});
+  });
 
-// pagination
-
-
-
-paginationTags[0].addEventListener("click", function() {
-
-	removeActiveClass()
-
-	currentSlide = 0
-
-	addActiveClass()
-	
-})
-
-paginationTags[1].addEventListener("click", function() {
-
-	removeActiveClass()
-
-	currentSlide = 1
-
-	addActiveClass()
-	
-})
-
-paginationTags[2].addEventListener("click", function() {
-
-	removeActiveClass()
-
-	currentSlide = 2
-
-	addActiveClass()
-	
-})
-
-
-// const goToSlide = function(slide){
-
-// }
-
-// paginationTags.forEach(function(tag) {
-//   tag.addEventListener("click", function() {
-
-//     goToSlide()
-
-//   });
-// });
+const goToSlide = function(tag){
+	removeActiveClass();
+	//subtract 1 from id because currentSlide starts at 0 but tag ids start at 1
+	currentSlide = tag.id - 1;
+	addActiveClass();
+}
